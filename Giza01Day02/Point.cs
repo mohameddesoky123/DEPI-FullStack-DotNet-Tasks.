16415f71bbf0c3c269488be749edfc5278165c8a
Mohamed Desoky <hesendesoky@gmail.com>
2026-08-27 14:48:52 +0300

    Add project files.

---
+++ Giza01Day02/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giza01Day02
+{
+    internal class Point
+    {
+        public int X; // 4 bytes
+        public int Y; // 4 bytes
+    }
+}
